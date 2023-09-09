6cc9a61188fdc600553f721f19516c60c5af7255
nerdthejohn <newtonjohn043@gmail.com>
2023-09-09 14:55:01 +0100

    chore: cleanup

--- apps/disbursements/models.py
+++ apps/disbursements/models.py
@@ -18,9 +18,7 @@ class Disbursement(models.Model):
         default=DisbursementFrequency.THIRTY_MINS,
     )
 
-    status = models.CharField(
-        'status', max_length=100, choices=DisbursementStatus.choices, default=DisbursementStatus.ACTIVE
-    )
+    status = models.CharField('status', choices=DisbursementStatus.choices, default=DisbursementStatus.ACTIVE, max_length=20)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
     next_run_timestamp = models.DateTimeField(null=True, blank=True)
