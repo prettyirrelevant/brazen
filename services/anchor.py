4b7c550e4438363ea45d997fc4adca05ecc43d69
Isaac Adewumi <ienioladewumi@gmail.com>
2023-09-09 11:45:11 +0100

    add api key to anchor client

--- services/anchor.py
+++ services/anchor.py
@@ -2,8 +2,9 @@ import requests
 
 
 class AnchorClient:
-    def __init__(self, base_url):
+    def __init__(self, base_url, api_key):
         self.base_url = base_url
+        self.api_key = api_key
         self.session = requests.Session()
 
     def create_customer(  # noqa: PLR0913
@@ -19,6 +20,7 @@ class AnchorClient:
     ) -> dict:
         response = self.session.post(
             url=f'{self.base_url}/api/v1/customers',
+            headers={'x-anchor-key': self.api_key},
             json={
                 'data': {
                     'type': 'IndividualCustomer',
@@ -44,6 +46,7 @@ class AnchorClient:
     def create_deposit_account(self, customer_id: str):
         response = self.session.post(
             url=f'{self.base_url}/api/v1/accounts',
+            headers={'x-anchor-key': self.api_key},
             json={
                 'data': {
                     'type': 'DepositAccount',
@@ -59,6 +62,7 @@ class AnchorClient:
     def create_counterparty(self, account_name: str, account_number: int, bank_id: str, bank_code: str):
         response = self.session.post(
             url=f'{self.base_url}/api/v1/counterparties',
+            headers={'x-anchor-key': self.api_key},
             json={
                 'data': {
                     'type': 'CounterParty',
@@ -86,6 +90,7 @@ class AnchorClient:
     def initiate_transfer(self, amount: int, reason: str, counterparty_id: str, account_id: str):
         response = self.session.post(
             url=f'{self.base_url}/api/v1/transfers',
+            headers={'x-anchor-key': self.api_key},
             json={
                 'data': {
                     'type': 'NIPTransfer',
@@ -107,13 +112,19 @@ class AnchorClient:
         return response.json()
 
     def get_banks(self):
-        response = self.session.get(f'{self.base_url}/api/v1/banks')
+        response = self.session.get(
+            url=f'{self.base_url}/api/v1/banks',
+            headers={'x-anchor-key': self.api_key},
+        )
         response.raise_for_status()
 
         return response.json()
 
-    def verify_account(self, account_number: int, bank_code: str):
-        response = self.session.get(f'{self.base_url}/api/v1/payments/verify-account/{bank_code}/{account_number}')
+    def verify_account(self, account_number: str, bank_code: str):
+        response = self.session.get(
+            url=f'{self.base_url}/api/v1/payments/verify-account/{bank_code}/{account_number}',
+            headers={'x-anchor-key': self.api_key},
+        )
         response.raise_for_status()
 
         return response.json()
