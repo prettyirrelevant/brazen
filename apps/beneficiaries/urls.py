9d6e7441a2b0859d1304bc6be97739c483451ab2
nerdthejohn <newtonjohn043@gmail.com>
2023-09-09 14:34:45 +0100

    chore: removed trailing slash

--- apps/beneficiaries/urls.py
+++ apps/beneficiaries/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from apps.beneficiaries.views import BeneficiaryView
 
-router = DefaultRouter(trailing_slash=True)
+router = DefaultRouter(trailing_slash=False)
 router.register('beneficiaries', BeneficiaryView, 'beneficiary')
 
 urlpatterns = [*router.urls]
--- apps/disbursements/urls.py
+++ apps/disbursements/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from apps.disbursements.views import DisbursementView
 
-router = DefaultRouter(trailing_slash=True)
+router = DefaultRouter(trailing_slash=False)
 router.register('disbursements', DisbursementView, 'disbursement')
 
 urlpatterns = [*router.urls]
